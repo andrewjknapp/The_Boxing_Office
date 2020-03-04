b15e2375899e13223ffdd26aedbe22c09dfc1c09
Ariel Khawly <akhawly59@gmail.com>
2020-03-04 13:16:53 -0800

    db creation and db connection

---
+++ app/config/config.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '', // what password do we use
+  database: 'users_db'
+});
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+});
+module.exports = connection;
