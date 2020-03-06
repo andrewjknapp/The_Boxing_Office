80ed07fa3ab0fd6115f048cbb86160a905a05d90
Armadillo <ajknapp2015@gmail.com>
2020-03-06 11:09:00 -0800

    Server up and running

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -3,34 +3,28 @@ let app = express();
 let path = require('path');
 // let models = require('../models');
 let sequlize = require('sequelize');
-<<<<<<< HEAD:app/routes/html-routes.js
 
 module.exports = function(app) {
 
-=======
-// index
+    // index
 app.get('/', function (req, res) {
-    return res.sendFile(path.join(__dirname, "public", "index.html")
+    return res.sendFile(path.join(__dirname, '..', "public", "index.html")
     )
 });
 // signup
 app.get('/signup', function (req, res) {
-    return res.sendFile(path.join(__dirname, "public", "signup.html")
+    return res.sendFile(path.join(__dirname, '..', "public", "signup.html")
     )
 });
->>>>>>> master:app/routes/html.routes.js
 // watchlist
 app.get('/watchlist', function (req, res) {
-    return res.sendFile(path.join(__dirname, "public", "watchlist.html")
+    return res.sendFile(path.join(__dirname, '..', "public", "watchlist.html")
     )
 });
 //battle
 app.get('/battle', function (req, res) {
-    return res.sendFile(path.join(__dirname, "public", "battle.html")
+    return res.sendFile(path.join(__dirname, '..', "public", "battle.html")
     )
 });
 
-<<<<<<< HEAD:app/routes/html-routes.js
 }
-=======
->>>>>>> master:app/routes/html.routes.js
