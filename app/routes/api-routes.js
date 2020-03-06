12b79d5a2abe460cb531a805a868a0408c48c6e6
Joel Spielberger <joelspielberger101@gmail.com>
2020-03-06 10:56:34 -0800

    added a user model and adjusted some API passport things

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,10 +1,9 @@
 let express = require('express');
 let app = express();
 let models = require('../models');
-let sequlize = require('sequelize');
 app.post('/api/users', function (req, res) {
     const [user, created] = await models.User.findOrCreate({
-        where: { username: req.body.username, password: req.body.password}
+        where: { username: req.body.username, name: req.body.name, password: req.body.password}
     });
     if (created) {
         res.send('Email already in use');
