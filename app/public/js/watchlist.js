c7c708a3a2e72116f6e247780d4538c7c6883d2b
Ariel Khawly <akhawly59@gmail.com>
2020-03-09 12:43:19 -0700

    all api routes in watchlist.js working

--- app/public/js/watchlist.js
+++ app/public/js/watchlist.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function () {
-
+    $('#toWatch').empty();
     $('#searchBtn').on('click', async function (event) {
         let search = $('#searchText').val();
         let data = await movieSearch(search);
@@ -11,31 +11,15 @@ $(document).ready(function () {
         $.ajax({
             url: queryURL
         }).then(function (response) {
-            if (response !== null) {
-                console.log(response);
-                let { Poster, Title, Year, Plot, imdbID } = response;
-                let currentMovie =
-                    `<section class='watchlistInfo'>
-                        <div class="poster" style="background-image: url(${Poster});"></div>
-                        <div class="title"><h3>${Title}  (${Year})</h3></div>
-                        <div class="plot">${Plot}</div>
-                        <div class="watched" style="background-image: url(assets/Popcorn.png);"></div>
-                        <div class="delete"> <button class="delete">Delete</button> </div>
-                        </section>`;
-
-                $('#toWatch').prepend(currentMovie);
-                let movieObj = {
-                    movieid: imdbID,
-                    poster: Poster
-                }
-                console.log(movieObj);
-                $.post("/api/watchlist", movieObj)
-                //loop through movie ids and call moviesearch 
-                // $.get("/api/watchlist")
-                //     .then(function (result) {
-                //         console.log(result);
-                //     });
+            let { Poster, Title, Year, Plot, imdbID } = response;
+            console.log(Title);
+            addToWatchlist(response);
+            let movieObj = {
+                movieid: imdbID,
+                poster: Poster
             }
+            console.log(movieObj);
+            $.post("/api/watchlist", movieObj)
         });
     };
     $('#watched').on('click', function () {
@@ -46,7 +30,37 @@ $(document).ready(function () {
             console.log(response);
         })
     });
-    $('#delete').on('click', function () {
+    //loop through movie ids and call moviesearch 
+    $.get("/api/watchlist")
+        .then(function (result) {
+            console.log(result);
+            for (i = 0; i < result.length; i++) {
+                let movieID = result[i].movieid;
+                let queryURL = `http://omdbapi.com/?apikey=trilogy&i=${movieID}`;
+                $.ajax({
+                    url: queryURL
+                }).then(function (response) {
+                    if (response !== null) {
+                        console.log(response);
+                        let { Poster, Title, Year, Plot, imdbID } = response;
+                        console.log(Title);
+                        let currentMovie =
+                            `<section class='watchlistInfo'>
+                        <div class="poster" style="background-image: url(${Poster});"></div>
+                        <div class="title"><h3>${Title}  (${Year})</h3></div>
+                        <div class="plot">${Plot}</div>
+                        <div class="watched" style="background-image: url(assets/Popcorn.png);"></div>
+                        <div class="delete"> <button id=${imdbID} class="delete">Delete</button> </div>
+                        </section>`;
+
+                        $('#toWatch').prepend(currentMovie);
+                    }
+                });
+            }
+        });
+    $(document.body).on('click', '.delete', function (event) {
+        $(this).parent().parent().remove();
+        let imdbID = event.target.getAttribute('id');
         $.ajax({
             method: "DELETE",
             url: "/api/watchlist/" + imdbID
@@ -54,12 +68,28 @@ $(document).ready(function () {
             console.log(response);
         });
     });
-    $(document.body).on('click', '.delete', function () {
-        $(this).parent().parent().remove();
-    });
 
     $(document.body).on('click', '.watched', function () {
         let movie = $(this).parent().detach();
         $('#seen').prepend(movie);
     });
+    function addToWatchlist(response) {
+        if (response !== null) {
+
+            console.log(response);
+            let { Poster, Title, Year, Plot, imdbID } = response;
+            console.log(Title);
+            let currentMovie =
+                `<section class='watchlistInfo'>
+                <div class="poster" style="background-image: url(${Poster});"></div>
+                <div class="title"><h3>${Title}  (${Year})</h3></div>
+                <div class="plot">${Plot}</div>
+                <div class="watched" style="background-image: url(assets/Popcorn.png);"></div>
+                <div class="delete"> <button id=${imdbID} class="delete">Delete</button> </div>
+                </section>`;
+
+            $('#toWatch').prepend(currentMovie);
+        }
+    }
+
 });
